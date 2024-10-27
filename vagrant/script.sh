f855f5a40e52cf473baf33ee76aa9517ab69a425
Ward Smeyers <145200525+Ward-Smeyers@users.noreply.github.com>
2024-10-27 11:01:47 +0100

    ✨ Update nginx.conf to listen on port 443 with SSL, and refactor script.sh for better readability and clarity.

--- vagrant/script.sh
+++ vagrant/script.sh
@@ -7,29 +7,29 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add the repository to APT sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
-# install docker tools
+# Install Docker tools
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# login to docker hub, recwiers .docker_password file vagrant synced_folder
+# Log in to Docker Hub, requires .docker_password file in Vagrant synced_folder
 cat /vagrant/.docker_password | docker login --username automator2 --password-stdin
 
-# add docker group and add vagrant user to docker group
+# Add Docker group and add Vagrant user to Docker group
 newgrp docker
-usermod -aG docker vagrant
+usermod -aG docker vagrant # add user to group
 groups vagrant
 
-# Uncomment to create new certificates. Requires cloudflare api key in .env file
-# # create certificates
+# Uncomment to create new certificates. Requires Cloudflare API key in .env file
+# # Create certificates
 # docker run -v /docker/lego:/opt/lego --env-file /docker/.env goacme/lego:latest --email=Ward.Smeyers@student.thomasmore.be --dns cloudflare --path=/opt/lego --domains milestone1.smeyers.xyz run
 # openssl x509 -in lego/certificates/milestone1.smeyers.xyz.crt -noout -text | head -15
 # docker rm -f $(docker ps -f status=exited -q)
 
-# start docker compose
+# Start Docker Compose (-f file) (-d detached)
 docker compose -f /docker/compose.yaml up -d
